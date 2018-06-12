1054a86edf5874d0a2e7cf91118292a8057b7be4
Lime Parallelogram <Lime-Parallelogram@users.noreply.github.com>
2018-06-12 19:55:37 +0100

    Update Email.Tutorial.py

--- Email.Tutorial.py
+++ Email.Tutorial.py
@@ -7,17 +7,17 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.IN)
 
 msg = MIMEMultipart()
-msg['From'] = 'raspberry11.250@gmail.com'
-msg['To'] = 'will@44ld.co.uk'
+msg['From'] = ''
+msg['To'] = ''
 msg['Subject'] = 'PANIC!!!'
 body = 'SOMEONE PRESSED THE BUTTON!!!'
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
-server.login('raspberry11.250@gmail.com','Forest Gold')
+server.login('','')
 
 while True:
     if GPIO.input(7) == 0:
         msg.attach(MIMEText(body, 'plain'))
-        server.sendmail('raspberry11.250@gmail.com','will@44ld.co.uk',msg.as_string())
+        server.sendmail('','',msg.as_string())
